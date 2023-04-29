c30d9d56b1dc924ea2e10bbe728fab2338c1d7d8
ajboots <ajboots@ncsu.edu>
2023-04-29 19:27:18 -0400

    added function tracker to webpage
    this trackers has many sin waves and interesting patterns in the sketch can be predicted by where these sin waves overlap.
    
    also added some to be placed sketch variants

--- Arcs.js
+++ Arcs.js
@@ -1,40 +1,92 @@
 
 // save this file as sketch.js
 // Sketch One
-var arcs = function (p) { // p could be any variable name
-  var SPEED = 1 / 2000.0
+
+function updateCurrentTime(time) {
+  var currentTimeElement = document.getElementById("current_time");
+  currentTimeElement.innerHTML = parseFloat(time.toFixed(3));;
+}
+
+var orb = function (p) { // p could be any variable name
+  var SPEED =  1/100.0
+
   p.setup = function() {
     p.colorMode(p.HSB)
     p.createCanvas(p.size, p.size);
     p.background(0)
   }
   //draw pattern every frame
-  p.draw = function() { p.drawArcs(); }
+  p.draw = function() { p.clear(); p.drawArcs(); }
 
   p.drawArcs = function() {
     //starting at the edge of the screen, draw an arc then move inward
     for (radius = p.size; radius > 1; radius -= p.arc_spacing) {
-      //color gradient, faded edge, and the pattern slowly growing at the start
-      let fadedEdge = p.min(p.millis() / 50, p.size) - (p.pow(radius, 5) / p.pow(radius, 4));
-      p.fill(p.color((radius * 100 / p.size) + 250, 300, fadedEdge));
+      //gradient, faded edge, and the pattern slowly growing at the start
+      p.fill((radius * 100 / p.size) + 250, 75, 300, p.alpha);
       //defines and draws arc from time and arc distance from center
-      angle = (radius) * (p.millis() * p.PI * SPEED / p.size)
+      updateCurrentTime(p.millis() * SPEED / p.size);
+      angle = radius * (p.millis() * SPEED / p.size)
       p.arc(p.size / 2, p.size / 2, radius, radius, -angle, angle - p.PI);
     }
   }
 };
-var ss = new p5(arcs, 'ss');
-ss.size = 200;
-ss.arc_spacing = 50;
+var snail = function (p) { // p could be any variable name
+  var SPEED =  1/100.0
+  p.setup = function() {
+    p.colorMode(p.HSB)
+    p.createCanvas(p.size, p.size);
+    p.background(0)
+  }
+  //draw pattern every frame
+  p.draw = function() { p.clear(); p.drawArcs(); }
 
-var sd = new p5(arcs, 'sd');
-sd.size = 400;
-sd.arc_spacing = 5;
+  p.drawArcs = function() {
+    //starting at the edge of the screen, draw an arc then move inward
+    for (radius = p.size; radius > 1; radius -= p.arc_spacing) {
+      //gradient, faded edge, and the pattern slowly growing at the start
+      p.fill((radius * 100 / p.size) + 250, 75, 300, p.alpha);
+      //defines and draws arc from time and arc distance from center
+      angle = radius * (p.millis() * SPEED / p.size)
+      p.arc(p.size / 2, p.size / 2, radius, radius, 0, angle - p.PI);
+    }
+  }
+};
+var pinwheel = function (p) { // p could be any variable name
+  var SPEED =  1/100.0
+  p.setup = function() {
+    p.colorMode(p.HSB)
+    p.createCanvas(p.size, p.size);
+    p.background(0)
+  }
+  //draw pattern every frame
+  p.draw = function() { p.clear(); p.drawArcs(); }
 
-var bs = new p5(arcs, 'bs');
-bs.size = 1200;
-bs.arc_spacing = 50;
+  p.drawArcs = function() {
+    //starting at the edge of the screen, draw an arc then move inward
+    for (radius = p.size; radius > 1; radius -= p.arc_spacing) {
+      //gradient, faded edge, and the pattern slowly growing at the start
+      p.fill((radius * 100 / p.size) + 250, 75, 300, p.alpha);
+      //defines and draws arc from time and arc distance from center
+      angle = radius * (p.millis() * SPEED / p.size)
+      p.arc(p.size / 2, p.size / 2, radius, radius, angle, angle + p.PI);
+    }
+  }
+};
 
-var bd = new p5(arcs, 'sd');
-bd.size = 600;
-bd.arc_spacing = 5;
+
+var main = new p5(orb, 'main');
+main.size = 800;
+main.arc_spacing = 3;
+main.alpha = 1;
+
+// var sd = new p5(arcs, 'sd');
+// sd.size = 800;
+// sd.arc_spacing = 5;
+
+// var bs = new p5(arcs, 'bs');
+// bs.size = 800;
+// bs.arc_spacing = 50;
+
+// var bd = new p5(arcs, 'sd');
+// bd.size = 800;
+// bd.arc_spacing = 5;
