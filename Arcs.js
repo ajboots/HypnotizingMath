cdcbc431e79e611b754bf0ff6fa66de93eaf4d38
ajboots <ajboots@ncsu.edu>
2023-05-01 15:31:18 -0400

    moved around sketches

--- Arcs.js
+++ Arcs.js
@@ -1,5 +1,7 @@
 speed = 1 / 100000;
 
+
+
 function updateCurrentTime(time) {
   var currentTimeElement = document.getElementById("current_time");
   currentTimeElement.innerHTML = parseFloat(time.toFixed(3));;
@@ -25,37 +27,37 @@ var arcs = function (p) { // p could be any variable name
 };
 
 var orbFunc = function (p) { // p could be any variable name
-  p.fill((radius * 100 / p.size) + 0, 75, 300, p.alpha);
+  p.fill((radius * 100 / p.size) + 120, 75, 300, p.alpha);
   angle = radius * p.millis() * speed / (p.arc_spacing / 2);
   p.arc(p.size / 2, p.size / 2, radius, radius, -angle, angle - p.PI);
   updateCurrentTime(p.millis() * speed);
 };
 var snailFunc = function (p) { // p could be any variable name
-  p.fill((radius * 100 / p.size) + 75, 75, 300, p.alpha);
+  p.fill((radius * 100 / p.size) + 250, 75, 300, p.alpha);
   angle = (p.size - radius) * p.millis() * speed / (p.arc_spacing / 2);
-  p.arc(p.size / 2, p.size / 2, radius, radius, -angle, angle - p.PI);
+  p.arc(p.size / 2, p.size / 2, radius, radius, -angle * 2, (angle / 2) + p.PI);
 };
 var ballFunc = function (p) { // p could be any variable name
-  p.fill((radius * 100 / p.size) + 150, 75, 300, p.alpha);
+  p.fill((radius * 120 / p.size) -29, 75, 300, p.alpha);
   angle = (p.size - radius) * p.millis() * speed / (p.arc_spacing / 2);
   p.arc(p.size / 2, p.size / 2, radius, p.size, -angle, angle - p.PI);
 };
 var pinwheelFunc = function (p) { // p could be any variable name
-  p.fill((radius * 100 / p.size) + 225, 75, 300, p.alpha);
+  p.fill((radius * 100 / p.size) + 180, 75, 300, p.alpha);
   angle = radius * p.millis() * speed / (p.arc_spacing / 2);
   p.arc(p.size / 2, p.size / 2, radius, radius, angle, angle + p.PI);
 };
 var mixedFunc = function (p) { // p could be any variable name
-  p.fill((radius * 100 / p.size) + 250, 85, 300, p.alpha);
+  p.fill(((radius * 80 / p.size) + 320) %360, 85, 300, p.alpha);
   angle = radius * p.millis() * speed / (p.arc_spacing / 2);
-  p.arc(p.size / 2, p.size / 2, radius, radius, -angle*2, (angle/4) + p.PI);
+  p.arc(p.size / 2, p.size / 2, radius, radius, -angle * 2, (angle / 4) + p.PI);
 };
 
 var main = new p5(arcs, 'main');
 main.size = 1600;
-main.arc_spacing = 20;
-main.alpha = .15;
-main.arcFunc = mixedFunc;
+main.arc_spacing = 10;
+main.alpha = .25;
+main.arcFunc = snailFunc;
 
 var ball = new p5(arcs, 'ball');
 ball.size = 700;
@@ -63,11 +65,11 @@ ball.arc_spacing = 10;
 ball.alpha = .1;
 ball.arcFunc = ballFunc;
 
-var snail = new p5(arcs, 'snail');
-snail.size = 700;
-snail.arc_spacing = 10;
-snail.alpha = .3;
-snail.arcFunc = snailFunc;
+var conch = new p5(arcs, 'conch');
+conch.size = 700;
+conch.arc_spacing = 10;
+conch.alpha = .3;
+conch.arcFunc = mixedFunc;
 
 var pinwheel = new p5(arcs, 'pinwheel');
 pinwheel.size = 700;
