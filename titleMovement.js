eb325817b2e4d10c6f04537a0d79e54d0d56c8da
ajboots <ajboots@ncsu.edu>
2023-05-11 08:19:49 -0400

    title looks cooler now

--- titleMovement.js
+++ titleMovement.js
@@ -12,25 +12,26 @@
 // });
 window.addEventListener('DOMContentLoaded', function () {
     var cyanlogo = document.getElementById('cyanlogo');
-
-    window.addEventListener('mousemove', function (event) {
-        var mouseX = - event.clientX;
-        var mouseY = event.clientY;
-        var distance = mouseX + mouseY
-        var moveX = Math.floor(distance / -400);
-        var moveY = Math.floor(-distance / -400);
-        cyanlogo.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)';
-    });
-});
-window.addEventListener('DOMContentLoaded', function () {
     var redlogo = document.getElementById('redlogo');
-
     window.addEventListener('mousemove', function (event) {
-        var mouseX = - event.clientX;
-        var mouseY = event.clientY;
-        var distance = mouseX + mouseY
-        var moveX = Math.floor(distance / 400);
-        var moveY = Math.floor(-distance / 400);
-        redlogo.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)';
+        // var mouseX = - event.clientX - (this.window.innerWidth / 2);
+        var mouseY = event.clientY - (this.window.innerHeight / 2);
+        var mouseX = event.clientX - (this.window.innerWidth / 2); 
+        var moveX = Math.floor(mouseX / -100);
+        var moveY = Math.floor(-mouseY / -100);
+        cyanlogo.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)';
+        redlogo.style.transform = 'translate(' + -moveX + 'px, ' + -moveY + 'px)';
     });
 });
+// window.addEventListener('DOMContentLoaded', function () {
+//     var redlogo = document.getElementById('redlogo');
+
+//     window.addEventListener('mousemove', function (event) {
+//         var mouseX = - event.clientX - (this.window.innerWidth / 2);
+//         var mouseY = event.clientY - (this.window.innerHeight / 2);
+//         var distance = mouseX + mouseY
+//         var moveX = Math.floor(distance / 400);
+//         var moveY = Math.floor(-distance / 400);
+//         redlogo.style.transform = 'translate(' + moveX + 'px, ' + moveY + 'px)';
+//     });
+// });
